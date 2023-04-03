b431f83a8910288b681b5e2ce6ff5356aef2ad30
Utsav Shah <120706939+utsavshahm@users.noreply.github.com>
2023-04-03 09:42:13 +0530

    Add files via upload

---
+++ binary.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+     //refer to image for binary system
+
+
+    // binary me base 2 hota he: 
+
+    // 45= 2^5 *1 + 2^4 *0 + 2^3 *1+ 2^2*1 + 2^1 *0 + 2^0 *1 = 101101
+
+    int a=10;
+    cout<<10;
+
+}
